3198cd7f6269e81d60018b27c7fc8227d3d672e1
jhon S <jhons@jhonui-MacBookPro.local>
2025-03-12 23:13:43 +0900

    [FEAT]: 리액트 타입스크립트 적용 및 디렉토리 구조 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Main from './pages/main/Main';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>
 );
 
---
+++ src/pages/main/Main.tsx
@@ -0,0 +1,14 @@
+import './Main.css';
+
+function Main() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>MyComponents</h1>
+        <h1></h1>
+      </header>
+    </div>
+  );
+}
+
+export default Main;
